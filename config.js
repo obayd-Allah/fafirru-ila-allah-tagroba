93ce6bcb92e4413c38c21f1d36a47b9d00da51c4
obayd-Allah <heba20268484@gmail.com>
2026-07-25 00:14:56 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,26 @@
-import { MOSQUES } from "./mosques.js";
-
 export const APP_CONFIG = {
 
     mosqueId: "mosque_001",
 
-    mosque: MOSQUES["mosque_001"]
+    mosques: {
+
+        mosque_001: {
+
+            id: "mosque_001",
+
+            name: "ففروا إلى الله",
+
+            shortName: "ففروا",
+
+            logo: "new logo.png",
+
+            title: "title.png"
+
+        }
+}
 
 };
+
+// بيانات المسجد الحالي
+export const CURRENT_MOSQUE =
+    APP_CONFIG.mosques[APP_CONFIG.mosqueId];
