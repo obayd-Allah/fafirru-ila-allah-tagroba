4f6edd0989014d7b19f3176dc8ccdc23b68c0b64
obayd-Allah <heba20268484@gmail.com>
2026-07-24 16:02:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -25,8 +25,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-const CURRENT_MOSQUE_ID = "fafirru-ila-allah";
-
 let students = [];
 let currentFilter = "boys";
 let rewardSending = false;
