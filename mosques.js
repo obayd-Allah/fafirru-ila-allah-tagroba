18f9cee21323d3304dbdcb1bb275c5cdaef51ce4
obayd-Allah <heba20268484@gmail.com>
2026-07-25 00:04:45 +0300

    Create mosques.js

---
+++ mosques.js
@@ -0,0 +1,11 @@
+export const MOSQUES = {
+
+    mosque_001: {
+        id: "mosque_001",
+        name: "ففروا إلى الله",
+        shortName: "ففروا",
+        logo: "new logo.png",
+        title: "title.png"
+    }
+
+};
