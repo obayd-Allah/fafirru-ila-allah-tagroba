531fc0ac178fd890bb86e4a21de492fbf66ed177
obayd-Allah <heba20268484@gmail.com>
2026-08-07 15:42:52 +0300

    Update mosqueFallback.js

--- mosqueFallback.js
+++ mosqueFallback.js
@@ -2,8 +2,8 @@ export const DEFAULT_MOSQUE = {
     id: "default",
     name: "مسابقة القرآن",
     studentsPageTitle: "لوحة نقاط التلاميذ",
-    logo: "images/default-logo.png",
-    title: "images/default-title.png",
+    logo: "error.jpg",
+    title: "",
     theme: "error",
     whatsapp: ""
 };
